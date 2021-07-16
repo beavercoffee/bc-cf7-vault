a9939432ae005f08d9f484b15802d6ef2067f110
beavercoffee <beavercoffee@vidsoe.com>
2021-07-15 22:51:21 -0600

    BC

--- classes/class-bc-cf7-vault.php
+++ classes/class-bc-cf7-vault.php
@@ -37,7 +37,7 @@ if(!class_exists('BC_CF7_Vault')){
     	//
     	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-        private $file = '', $post_id = 0;
+        private $additional_data = [], $file = '', $post_id = 0;
 
     	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -89,6 +89,13 @@ if(!class_exists('BC_CF7_Vault')){
             add_action('wpcf7_before_send_mail', [$this, 'wpcf7_before_send_mail'], 10, 3);
             add_action('wpcf7_mail_failed', [$this, 'wpcf7_mail_failed']);
             add_action('wpcf7_mail_sent', [$this, 'wpcf7_mail_sent']);
+            add_filter('wpcf7_posted_data', [$this, 'wpcf7_posted_data']);
+            add_filter('wpcf7_posted_data_checkbox', [$this, 'wpcf7_posted_data_type'], 10, 3);
+            add_filter('wpcf7_posted_data_checkbox*', [$this, 'wpcf7_posted_data_type'], 10, 3);
+            add_filter('wpcf7_posted_data_radio', [$this, 'wpcf7_posted_data_type'], 10, 3);
+            add_filter('wpcf7_posted_data_radio*', [$this, 'wpcf7_posted_data_type'], 10, 3);
+            add_filter('wpcf7_posted_data_select', [$this, 'wpcf7_posted_data_type'], 10, 3);
+            add_filter('wpcf7_posted_data_select*', [$this, 'wpcf7_posted_data_type'], 10, 3);
             if(!has_filter('wpcf7_verify_nonce', 'is_user_logged_in')){
                 add_filter('wpcf7_verify_nonce', 'is_user_logged_in');
             }
@@ -154,6 +161,44 @@ if(!class_exists('BC_CF7_Vault')){
                     update_post_meta($this->post_id, 'bc_submission_status', $submission->get_status());
                 }
             }
+        }
+
+    	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        public function wpcf7_posted_data($posted_data){
+            if($this->additional_data){
+                $posted_data = array_merge($posted_data, $this->additional_data);
+            }
+            return $posted_data;
+        }
+
+    	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        public function wpcf7_posted_data_type($value, $value_orig, $tag){
+			$name = $tag->name;
+            $pipes = $tag->pipes;
+            $type = $tag->type;
+			if(wpcf7_form_tag_supports($type, 'selectable-values')){
+                $value = (array) $value;
+                $value_orig = (array) $value_orig;
+				if($tag->has_option('free_text')){
+        			$last_val = array_pop($value);
+					list($tied_item) = array_slice(WPCF7_USE_PIPE ? $tag->pipes->collect_afters() : $tag->values, -1, 1);
+					$tied_item = html_entity_decode($tied_item, ENT_QUOTES, 'UTF-8');
+					if(strpos($last_val, $tied_item) === 0){
+						$value[] = $tied_item;
+						$this->additional_data[$name . '_free_text'] = trim(str_replace($tied_item, '', $last_val));
+					} else {
+						$value[] = $last_val;
+						$this->additional_data[$name . '_free_text'] = '';
+					}
+                }
+            }
+			if(WPCF7_USE_PIPE and $pipes instanceof WPCF7_Pipes and !$pipes->zero()){
+				$this->additional_data[$name . '_value'] = $value;
+				$value = $value_orig;
+            }
+            return $value;
         }
 
     	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
