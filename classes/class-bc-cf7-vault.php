5a0f0fb662fc8c40ec69028ec63aac1aba139165
beavercoffee <beavercoffee@vidsoe.com>
2021-07-17 13:47:14 -0600

    BC

--- classes/class-bc-cf7-vault.php
+++ classes/class-bc-cf7-vault.php
@@ -37,7 +37,7 @@ if(!class_exists('BC_CF7_Vault')){
     	//
     	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-        private $additional_data = [], $file = '', $post_id = 0;
+        private $file = '', $post_id = 0;
 
     	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -47,23 +47,43 @@ if(!class_exists('BC_CF7_Vault')){
 
     	private function __construct($file = ''){
             $this->file = $file;
-            add_action('plugins_loaded', [$this, 'plugins_loaded']);
+            add_action('bc_cf7_loaded', [$this, 'bc_cf7_loaded']);
+            add_action('bc_functions_loaded', [$this, 'bc_functions_loaded']);
         }
 
     	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    	private function upload_file($tmp_name = '', $post_id = 0){
-            $file = bc_move_uploaded_file($tmp_name);
-            if(is_wp_error($file)){
-                return $file;
-            }
-            return bc_upload($file, $post_id);
+    	private function is_type($contact_form = null){
+            return bc_cf7()->is_type('', $contact_form);
         }
 
     	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     	//
     	// public
     	//
+    	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        public function bc_cf7_loaded(){
+            add_action('init', [$this, 'init']);
+            add_action('wpcf7_before_send_mail', [$this, 'wpcf7_before_send_mail'], 10, 3);
+            add_action('wpcf7_mail_failed', [$this, 'wpcf7_mail_failed']);
+            add_action('wpcf7_mail_sent', [$this, 'wpcf7_mail_sent']);
+            if(!has_filter('wpcf7_verify_nonce', 'is_user_logged_in')){
+                add_filter('wpcf7_verify_nonce', 'is_user_logged_in');
+            }
+        }
+
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        public function bc_functions_loaded(){
+            bc_build_update_checker('https://github.com/beavercoffee/bc-cf7-vault', $this->file, 'bc-cf7-vault');
+            if(!bc_is_plugin_active('bc-cf7/bc-cf7.php')){
+                add_action('admin_notices', function(){
+                    echo bc_admin_notice(sprintf(__('No plugins found for: %s.'),'<strong>BC CF7</strong>'));
+                });
+        	}
+        }
+
     	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
         public function init(){
@@ -78,37 +98,11 @@ if(!class_exists('BC_CF7_Vault')){
 
         // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-        public function plugins_loaded(){
-            if(!defined('BC_FUNCTIONS')){
-        		return;
-        	}
-            if(!defined('WPCF7_VERSION')){
-        		return;
-        	}
-            add_action('init', [$this, 'init']);
-            add_action('wpcf7_before_send_mail', [$this, 'wpcf7_before_send_mail'], 10, 3);
-            add_action('wpcf7_mail_failed', [$this, 'wpcf7_mail_failed']);
-            add_action('wpcf7_mail_sent', [$this, 'wpcf7_mail_sent']);
-            add_filter('wpcf7_posted_data', [$this, 'wpcf7_posted_data']);
-            add_filter('wpcf7_posted_data_checkbox', [$this, 'wpcf7_posted_data_type'], 10, 3);
-            add_filter('wpcf7_posted_data_checkbox*', [$this, 'wpcf7_posted_data_type'], 10, 3);
-            add_filter('wpcf7_posted_data_radio', [$this, 'wpcf7_posted_data_type'], 10, 3);
-            add_filter('wpcf7_posted_data_radio*', [$this, 'wpcf7_posted_data_type'], 10, 3);
-            add_filter('wpcf7_posted_data_select', [$this, 'wpcf7_posted_data_type'], 10, 3);
-            add_filter('wpcf7_posted_data_select*', [$this, 'wpcf7_posted_data_type'], 10, 3);
-            if(!has_filter('wpcf7_verify_nonce', 'is_user_logged_in')){
-                add_filter('wpcf7_verify_nonce', 'is_user_logged_in');
-            }
-            bc_build_update_checker('https://github.com/beavercoffee/bc-cf7-vault', $this->file, 'bc-cf7-vault');
-        }
-
-        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
         public function wpcf7_before_send_mail($contact_form, &$abort, $submission){
             if($contact_form->is_true('do_not_store')){
                 return;
             }
-            if('' !== bc_cf7_type($contact_form)){
+            if(!$this->is_type($contact_form)){
                 return;
             }
             if(!$submission->is('init')){
@@ -126,9 +120,7 @@ if(!class_exists('BC_CF7_Vault')){
                 return;
             }
             $this->post_id = $post_id;
-            bc_cf7_update_meta_data(bc_cf7_meta_data($contact_form, $submission), $post_id);
-            bc_cf7_update_posted_data($submission->get_posted_data(), $post_id);
-            bc_cf7_update_uploaded_files($submission->uploaded_files(), $post_id);
+            bc_cf7()->update($contact_form, $submission, 'post', $post_id);
             do_action('bc_cf7_vault', $post_id, $contact_form, $submission);
             // continue to mail_sent or mail_failed action
         }
@@ -161,44 +153,6 @@ if(!class_exists('BC_CF7_Vault')){
                     update_post_meta($this->post_id, 'bc_submission_status', $submission->get_status());
                 }
             }
-        }
-
-    	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-        public function wpcf7_posted_data($posted_data){
-            if($this->additional_data){
-                $posted_data = array_merge($posted_data, $this->additional_data);
-            }
-            return $posted_data;
-        }
-
-    	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-        public function wpcf7_posted_data_type($value, $value_orig, $tag){
-			$name = $tag->name;
-            $pipes = $tag->pipes;
-            $type = $tag->type;
-			if(wpcf7_form_tag_supports($type, 'selectable-values')){
-                $value = (array) $value;
-                $value_orig = (array) $value_orig;
-				if($tag->has_option('free_text')){
-        			$last_val = array_pop($value);
-					list($tied_item) = array_slice(WPCF7_USE_PIPE ? $tag->pipes->collect_afters() : $tag->values, -1, 1);
-					$tied_item = html_entity_decode($tied_item, ENT_QUOTES, 'UTF-8');
-					if(strpos($last_val, $tied_item) === 0){
-						$value[] = $tied_item;
-						$this->additional_data[$name . '_free_text'] = trim(str_replace($tied_item, '', $last_val));
-					} else {
-						$value[] = $last_val;
-						$this->additional_data[$name . '_free_text'] = '';
-					}
-                }
-            }
-			if(WPCF7_USE_PIPE and $pipes instanceof WPCF7_Pipes and !$pipes->zero()){
-				$this->additional_data[$name . '_value'] = $value;
-				$value = $value_orig;
-            }
-            return $value;
         }
 
     	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
